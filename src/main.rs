905b7ae142fa7716088e4b4c7b12802d8dd5bf83
Jeff Rade <jeffrade@gmail.com>
2020-05-17 20:41:26 -0500

    Convert ciphertext Vec<u8> to String representation of Hexadecimal for transmit

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate rsa;
 extern crate aes_soft as aes;
 extern crate block_modes;
 extern crate simple_logger;
+extern crate hex;
 
 use std::convert::From;
 use std::convert::Into;
@@ -27,23 +28,9 @@ use rsa::{PublicKey, RSAPublicKey, RSAPrivateKey, PaddingScheme};
 
 //PKCS1 vs PKCS8 https://stackoverflow.com/questions/48958304/pkcs1-and-pkcs8-format-for-rsa-private-key
 fn main() {  
-  simple_logger::init_by_env(); //Defaults to ERROR, set by exporting RUST_LOG
-
   let dir = String::from(env!("HOME")) + "/.slackrypt";
-  let key_file = String::from(&dir) + "/key.pem";
-
-  match fs::create_dir(&dir) {
-    Ok(_) => true,
-    Err(_) => {
-      warn!("Ignore since ~/.slackrypt dir might already exist.");
-      true
-    }
-  };
-
-  if !keys_exist(&key_file) {
-    create_keys(&key_file).unwrap();
-  }
-
+  init(&dir);
+  
   let private_key = get_private_key(&dir).unwrap();
   let public_key: RSAPublicKey = private_key.into();
   let public_key_openssl: RSAPublicKey = get_public_key(&dir).unwrap();
@@ -67,8 +54,18 @@ fn main() {
 
   type Aes128Cbc = Cbc<Aes128, Pkcs7>;
   let cipher = Aes128Cbc::new_var(&key, &iv).unwrap();
-  let ciphertext = cipher.encrypt_vec(&message);
-  info!("ciphertext is {}", String::from_utf8_lossy(&ciphertext).to_string());
+  let ciphertext: Vec<u8> = cipher.encrypt_vec(&message);
+
+  //Ciphertext to hexadecimal
+  let mut ciphertext_hex: String = String::new();
+  for i in 0..ciphertext.len() {
+    let u_8: u8 = ciphertext[i];
+    let v_i: String = format!("{:02X}", u_8);//println!("{} : {:02X}", u_8, u_8);
+    ciphertext_hex.push_str(&v_i);
+  }
+  info!("ciphertext_hex is {}", &ciphertext_hex);
+  let ciphertext_decoded: Vec<u8> = hex::decode(ciphertext_hex).expect("hex decoding failed!");
+  assert_eq!(ciphertext, ciphertext_decoded);
 
   //RSA key(and iv) encryption
   let cipher_vec_key = encrypt_data(&key, &public_key);
@@ -86,7 +83,7 @@ fn main() {
 
   //AES message decryption
   let cipher = Aes128Cbc::new_var(&key_vec, &iv).unwrap();
-  let mut buf = ciphertext.to_vec();
+  let mut buf: Vec<u8> = ciphertext_decoded.to_vec();
   let decrypted_ciphertext: &[u8] = cipher.decrypt(&mut buf).unwrap();
   assert_eq!(decrypted_ciphertext, message.as_slice());
   info!("decrypted message is {}", String::from_utf8_lossy(&message).to_string());
@@ -138,23 +135,6 @@ fn get_private_key(dir: &str) -> Result<RSAPrivateKey> {
   Ok(private_key)
 }
 
-fn keys_exist(key_file: &str) -> bool {
-  Path::new(key_file).exists()
-}
-
-fn create_keys(key_file: &str) -> Result<()> {
-  let bits_str = String::from(env!("SCRYPT_KEY_SIZE")); //Set this to min of 2048
-  let bits: i32 = bits_str.parse::<i32>().unwrap();
-  info!("Creating {} bit keys, this may take a while...", bits);
-
-  // Using openssl since RustCrypto/RSA cannot export keys in PEM.
-  // See issue https://github.com/RustCrypto/RSA/issues/31
-  openssl_generate(key_file, bits);
-  chmod_file(key_file, "0400");
-  openssl_pub_key_out(key_file);
-  Ok(())
-}
-
 // openssl rsa -in test_key.pem -outform PEM -pubout -out test_key.pem.pub
 fn openssl_pub_key_out(file_name: &str) {
   let mut pub_key_file = String::from(file_name);
@@ -170,7 +150,7 @@ fn openssl_pub_key_out(file_name: &str) {
     .arg(&pub_key_file)
     .output()
     .expect("Failed to generate keys!");
-  info!("openssl rsa status {}", cmd.status);
+  debug!("openssl rsa status {}", cmd.status);
   debug!("openssl rsa stdout {}", String::from_utf8_lossy(&cmd.stdout));
   debug!("openssl rsa stderr: {}", String::from_utf8_lossy(&cmd.stderr));
   chmod_file(&pub_key_file, "0644")
@@ -195,4 +175,38 @@ fn chmod_file(file_name: &str, permissions: &str) {
     .output()
     .expect("Failed to chmod file!");
   debug!("chmod cmd returned {}", cmd.status);
+}
+
+fn init(dir: &str) {
+  simple_logger::init_by_env(); //Defaults to ERROR, set by exporting RUST_LOG
+
+  match fs::create_dir(dir) {
+    Ok(_) => true,
+    Err(_) => {
+      warn!("Ignore since ~/.slackrypt dir might already exist.");
+      true
+    }
+  };
+
+  let key_file = String::from(dir) + "/key.pem";
+  if !keys_exist(&key_file) {
+    create_keys(&key_file).unwrap();
+  }
+}
+
+fn create_keys(key_file: &str) -> Result<()> {
+  let bits_str = String::from(env!("SCRYPT_KEY_SIZE")); //Set this to min of 2048
+  let bits: i32 = bits_str.parse::<i32>().unwrap();
+  info!("Creating {} bit keys, this may take a while...", bits);
+
+  // Using openssl since RustCrypto/RSA cannot export keys in PEM.
+  // See issue https://github.com/RustCrypto/RSA/issues/31
+  openssl_generate(key_file, bits);
+  chmod_file(key_file, "0400");
+  openssl_pub_key_out(key_file);
+  Ok(())
+}
+
+fn keys_exist(key_file: &str) -> bool {
+  Path::new(key_file).exists()
 }
