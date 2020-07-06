a71496dc9a0b9088f5b0ef494196996c960a3ffb
Jeff Rade <jeffrade@gmail.com>
2020-07-05 20:55:42 -0500

    Moving http server functionality to new 'server' module

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,35 +2,13 @@ use std::fs;
 use std::sync::mpsc;
 use std::thread;
 
-use actix_rt::System;
-use actix_web::{dev::Server, middleware, web, App, HttpResponse, HttpServer};
-use log::{debug, info, warn};
-use serde::{Deserialize, Serialize};
+use log::warn;
 
 mod util;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct User {
-    user: String,
-    pubkey: String,
-}
-
 mod db;
+mod server;
 mod slack;
-
-// curl -H "Content-Type: application/json" --request POST --data '{"user": "ctester", "pubkey": "from curl"}' http://127.0.0.1:8080/pubkey/upload
-async fn pubkey_upload(user: web::Json<User>) -> HttpResponse {
-    info!("payload: {:?}", &user);
-    db::insert(&user.0.user, &user.0.pubkey).unwrap();
-    HttpResponse::Ok().json(user.0)
-}
-
-// curl -H "Content-Type: application/json" http://127.0.0.1:8080/pubkey/users
-async fn pubkey_users() -> HttpResponse {
-    debug!("pubkey_users() entering...");
-    let users: Vec<String> = db::get_users_all().unwrap();
-    HttpResponse::Ok().json(users)
-}
 
 fn main() {
     simple_logger::init_by_env();
@@ -42,32 +20,11 @@ fn main() {
     let port: &str = "8080";
     let server: String = String::from(host) + ":" + port;
     thread::spawn(move || {
-        let _ = start_server(server, tx);
+        let _ = server::start_server(server, tx);
     });
     let _srv = rx.recv().unwrap();
 
     slack::init(); //This must be called last
-}
-
-// Inspiration from https://github.com/actix/examples/blob/e8ab9ee7cab3a17aedbddb4800d56d206d0a296f/run-in-thread/src/main.rs
-fn start_server(server: String, tx: mpsc::Sender<Server>) -> std::io::Result<()> {
-    info!("Starting HTTP service...");
-    let mut sys = System::new("slackrypt-server");
-
-    let srv = HttpServer::new(|| {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .service(web::resource("/pubkey/upload").route(web::post().to(pubkey_upload)))
-            .service(web::resource("/pubkey/users").route(web::get().to(pubkey_users)))
-    })
-    .bind(server)?
-    .run();
-
-    // send server controller to main thread
-    let _ = tx.send(srv.clone());
-
-    // run future
-    sys.block_on(srv)
 }
 
 fn init() {
---
+++ server/src/server.rs
@@ -0,0 +1,49 @@
+use std::sync::mpsc;
+
+use actix_rt::System;
+use actix_web::{dev::Server, middleware, web, App, HttpResponse, HttpServer};
+use log::{debug, info};
+use serde::{Deserialize, Serialize};
+
+use crate::db;
+
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    user: String,
+    pubkey: String,
+}
+
+// Inspiration from https://github.com/actix/examples/blob/e8ab9ee7cab3a17aedbddb4800d56d206d0a296f/run-in-thread/src/main.rs
+pub fn start_server(server: String, tx: mpsc::Sender<Server>) -> std::io::Result<()> {
+    info!("Starting HTTP service...");
+    let mut sys = System::new("slackrypt-server");
+
+    let srv = HttpServer::new(|| {
+        App::new()
+            .wrap(middleware::Logger::default())
+            .service(web::resource("/pubkey/upload").route(web::post().to(pubkey_upload)))
+            .service(web::resource("/pubkey/users").route(web::get().to(pubkey_users)))
+    })
+    .bind(server)?
+    .run();
+
+    // send server controller to main thread
+    let _ = tx.send(srv.clone());
+
+    // run future
+    sys.block_on(srv)
+}
+
+// curl -H "Content-Type: application/json" --request POST --data '{"user": "ctester", "pubkey": "from curl"}' http://127.0.0.1:8080/pubkey/upload
+async fn pubkey_upload(user: web::Json<User>) -> HttpResponse {
+    info!("payload: {:?}", &user);
+    db::insert(&user.0.user, &user.0.pubkey).unwrap();
+    HttpResponse::Ok().json(user.0)
+}
+
+// curl -H "Content-Type: application/json" http://127.0.0.1:8080/pubkey/users
+async fn pubkey_users() -> HttpResponse {
+    debug!("pubkey_users() entering...");
+    let users: Vec<String> = db::get_users_all().unwrap();
+    HttpResponse::Ok().json(users)
+}
